55bcae35a10a4493c61fb2048fc087fc0c2b4025
aram-arfam <arfam.alam@outlook.com>
2025-06-25 22:06:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.port || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://userauth-5z8s.onrender.com"];
 
 app.use(express.json());
 app.use(cookieParser());
